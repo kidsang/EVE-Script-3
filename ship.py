71479dfb6f6f4a61aa07a3b54ae5de77d32aa94c
kid <963262214@qq.com>
2013-05-26 21:24:31 +0800

    asdf

--- ship.py
+++ ship.py
@@ -1,4 +1,6 @@
+import time
 import shortcut as sc
+from util import *
 
 cache = {}
 
@@ -46,4 +48,47 @@ def Warping():
 	sr = GetShipSr()
 	eve.GetAttr(sr, 'speedStatus', '_')
 	return eve.GetString('_', 'text') == '<center>(Warping)'
+
+def GetSlot(s, i):
+	slot = 'shipui_slot_' + str(s) + '_' + str(i)
+	slotName = 'inFlight'
+	if s == 0:
+		slotName += 'LowSlot'
+	elif s == 1:
+		slotName += 'MediumSlot'
+	else:
+		slotName += 'HighSlot'
+	slotName += str(i + 1)
+
+	sr = GetShipSr()
+	eve.GetAttr(sr, 'slotsContainer', '_')
+	eve.FindChild('_', slotName, slot)
+	return slot
+
+def ClickSlot(slot):
+	eve.Click(slot, 30, 40)
+
+def SlotActivated(slot):
+	eve.FindChild(slot, 'glow', '_')
+	return eve.GetBool('_', 'display')
+
+def SlotBusy(slot):
+	eve.FindChild(slot, 'busy', '_')
+	return eve.GetBool('_', 'display')
+
+def TurnOnSlot(s, i):
+	Log('Turn on slot ' + str(s) + ' ' + str(i), 1)
+	slot = GetSlot(s, i)
+	if not SlotActivated(slot):
+		while SlotBusy(slot):
+			time.sleep(0.2)
+		ClickSlot(slot)
+	Log('end', -1)
+
+def TurnOffSlot(s, i):
+	Log('Turn off slot ' + str(s) + ' ' + str(i), 1)
+	slot = GetSlot(s, i)
+	while SlotActivated(slot):
+		ClickSlot(slot)
+	Log('end', -1)
 
